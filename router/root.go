d9056ae2c25c1a5e813e67723bfae0e68274230d
김도훈 <ehgns5668@naver.com>
2024-05-17 23:56:56 +0900

    [add] Add API Timeout Middleware

--- router/root.go
+++ router/root.go
@@ -1,14 +1,47 @@
 package router
 
-import "golang_db_study/config"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"golang_db_study/config"
+	"net/http"
+	"time"
+)
 
 type Router struct {
 	config *config.Config
+
+	engin *gin.Engine
 }
 
 func NewRouter(config *config.Config) (*Router, error) {
 	r := &Router{
 		config: config,
+		engin:  gin.New(),
 	}
-	return r, nil
+	r.engin.Use(requestTimeOutMiddleWare(5 * time.Second))
+
+	return r, r.engin.Run(config.ServerInfo.Port)
+}
+
+func requestTimeOutMiddleWare(timeout time.Duration) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c.Request.Context(), timeout)
+
+		defer cancel()
+
+		c.Request = c.Request.WithContext(ctx)
+		done := make(chan struct{})
+
+		go func() {
+			defer close(done)
+		}()
+
+		select {
+		case <-done:
+			return
+		case <-ctx.Done():
+			c.AbortWithStatusJSON(http.StatusGatewayTimeout, gin.H{"error": "Request Time out"})
+		}
+	}
 }
